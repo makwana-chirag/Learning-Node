595b133f40514a77ee34e3b271ed352546a8594f
chiragm <chiragm@webelight.co.in>
2025-06-12 15:13:04 +0530

    add and mutiple function added and learn about dev dependenvy

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ var _ = require("underscore");
 
 const result = _.contains([1, 2, 3, 4, 5], 4);
 
+module.export.add = function (a, b) {
+  return a + b;
+};
+
+module.export.mutiply = function (a, b) {
+  return a * b;
+};
+
 console.log("result", result);
